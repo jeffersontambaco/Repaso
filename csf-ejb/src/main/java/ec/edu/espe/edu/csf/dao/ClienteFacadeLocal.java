1c297e305d6457cb768fcce5320f6c400b6a869c
jeffe <jeffe@DESKTOP-CNFTUVG>
2017-07-12 09:19:47 -0500

    Eliminacion de comentarios de clientefacadelocal

--- csf-ejb/src/main/java/ec/edu/espe/edu/csf/dao/ClienteFacadeLocal.java
+++ csf-ejb/src/main/java/ec/edu/espe/edu/csf/dao/ClienteFacadeLocal.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ec.edu.espe.edu.csf.dao;
 
 import ec.edu.espe.edu.csf.model.Cliente;
 import java.util.List;
 import javax.ejb.Local;
 
-/**
- *
- * @author jeffe
- */
+
 @Local
 public interface ClienteFacadeLocal {
 
